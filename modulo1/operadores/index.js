9cd708ab341c2f2c160c0a3636b7c5ee30b771db
Luiz Rafhael <luizrafhael_1987@hotmail.com>
2022-04-14 23:08:31 -0300

    adicionar Index HTML js

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,136 @@
+//Exercicio de interpretação de codigo//
+/*const bool1 = true
+const bool2 = false
+const bool3 = !bool2 // (!) ele sempre retorna o oposto entao ele é true//
+
+let resultado = bool1 && bool2
+console.log("a. ", resultado)
+
+resultado = bool1 && bool2 && bool3 
+console.log("b. ", resultado) 
+
+resultado = !resultado && (bool1 || bool2) 
+console.log("c. ", resultado)
+
+console.log("d. ", typeof resultado)*/
+
+//false ,false ,true e boleano//
+
+//exercicio 2//
+
+/* let primeiroNumero = prompt("Digite um numero!")
+let segundoNumero = prompt("Digite outro numero!")
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) */
+
+////porque tem que trasformar  a String em Number//
+
+
+
+//exercicio 3//
+
+/*let primeiroNumero = Number (prompt("Digite um numero!"))
+let segundoNumero =  Number (prompt("Digite outro numero!"))
+
+const soma = primeiroNumero + segundoNumero
+
+console.log(soma) */
+
+
+
+// exercicio 1 a,b,c,d//
+
+ /*let idadeDoUsuario = Number (prompt("Qual a sua idade?"))
+let idadeMelhorAmigo = Number (prompt("Qual a idade do amigo?"))
+let maiorIdade = idadeDoUsuario >= idadeMelhorAmigo
+
+console.log("Nome Usuario", idadeDoUsuario)
+console.log("Nome Amigo", idadeMelhorAmigo)
+console.log("Qual idade é maior", maiorIdade) 
+console.log("Diferença de idade", idadeDoUsuario - idadeMelhorAmigo)
+*/
+
+//minha idade maior que do meu amigo , e a diferença da 10 anos)
+
+// exercicio 2//
+
+/* let restoDaDivisao = 40 % 2
+ 
+console.log(restoDaDivisao)
+ */
+// numeros pares ele fica a sobra 0 e numeros impar eles ficam sobra  1//
+
+
+
+// exercicio 3//
+
+/* let idadeEmAnos = Number (prompt("Qual sua idade em anos?"))
+let idadeEmMeses = Number (prompt("Qual sua idade em meses?"))
+let idadeEmDias = Number (prompt("Qual sua idade em dias?"))
+let idadeEmHoras = Number (prompt("Qual sua idade em horas?"))
+
+
+console.log(idadeEmAnos)
+console.log(idadeEmMeses)
+console.log(idadeEmDias)
+console.log(idadeEmHoras)
+ */
+
+// exercicio 4//
+
+/* let primeiroNumero = Number (prompt("Digite um Numero"))
+let segundoNumero = Number (prompt("Digite outro numero"))
+console.log(primeiroNumero)
+console.log(segundoNumero)
+
+const primeiro = 34
+const segundo = 6
+
+let comparacao = primeiro > segundo
+console.log(comparacao)
+
+let igual = primeiro === segundo
+console.log(igual)
+
+let divisível = primeiro % segundo ===0
+console.log(divisível)
+
+let divisível2 = segundo % primeiro ===0
+console.log(divisível2)
+
+
+
+
+
+
+
+
+/* O primeiro numero é maior que segundo? true
+O primeiro numero é igual ao segundo? false
+O primeiro numero é divisível pelo segundo? true
+O segundo numero é divisível pelo primeiro? true */
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
