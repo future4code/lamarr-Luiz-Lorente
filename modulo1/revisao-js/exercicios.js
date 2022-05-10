87df174a27f3cb6e9658df6040e55a9b09f3b657
Luiz Rafhael <luizrafhael_1987@hotmail.com>
2022-05-09 22:48:55 -0300

    JavaProjeto

---
+++ modulo1/revisao-js/exercicios.js
@@ -0,0 +1,112 @@
+/* // ATENÇÃO!!!
+//    -> NÃO COMENTE NENHUMA DAS FUNÇÕES DECLARADAS!!! 
+//    -> NÃO MODIFIQUE OS PARÂMETROS DAS FUNÇÕES!!! ()
+
+
+// EXERCÍCIO 01
+function retornaTamanhoArray(array) {
+    const primeiro = array.length
+
+    return primeiro
+
+}
+console.log(retornaTamanhoArray)
+
+// EXERCÍCIO 02
+function retornaArrayInvertido(array) {
+ const invertida =   array.reverse();
+ return invertida
+
+}
+console.log(retornaArrayInvertido)
+
+// EXERCÍCIO 03
+function retornaArrayOrdenado(array) {
+    const crescente = array
+    return crescente.sort((a,b) => a-b)
+
+ 
+        
+    }
+console.log(retornaArrayOrdenado) */
+
+// EXERCÍCIO 04
+/* function retornaNumerosPares(array) {
+     const pares = array
+     return pares.filter((array)=>(array%2)===0)
+
+
+}
+ */
+
+// EXERCÍCIO 05
+function retornaNumerosParesElevadosADois(array) {
+    if(array *2 ===0){
+        return "par" 
+    }else {
+        
+    }
+
+    
+ 
+}
+
+// EXERCÍCIO 06
+function retornaMaiorNumero(array) {
+  
+}
+
+// EXERCÍCIO 07
+function retornaObjetoEntreDoisNumeros(num1, num2) {
+
+}
+
+// EXERCÍCIO 08
+function retornaNPrimeirosPares(n) {
+   
+}
+
+// EXERCÍCIO 09
+function classificaTriangulo(ladoA, ladoB, ladoC) {
+
+}
+
+// EXERCÍCIO 10
+function retornaSegundoMaiorESegundoMenor(array) {
+  
+}
+
+// EXERCÍCIO 11
+function retornaChamadaDeFilme(filme) {
+   
+}
+
+// EXERCÍCIO 12
+function retornaPessoaAnonimizada(pessoa) {
+   
+}
+
+// EXERCÍCIO 13A
+function retornaPessoasAutorizadas(pessoas) {
+   
+}
+
+// EXERCÍCIO 13B
+function retornaPessoasNaoAutorizadas(pessoas) {
+  
+}
+
+// EXERCÍCIO 14
+function retornaContasComSaldoAtualizado(contas) {
+
+}
+
+// EXERCÍCIO 15A
+function retornaArrayOrdenadoAlfabeticamente(consultas) {
+  
+}
+
+// EXERCÍCIO 15B
+function retornaArrayOrdenadoPorData(consultas) {
+   
+}
