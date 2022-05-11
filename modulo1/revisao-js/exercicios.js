9e1b1345a31f25d570b20a46b80c2b63c7c792a7
Luiz Rafhael <luizrafhael_1987@hotmail.com>
2022-05-11 19:06:40 -0300

    Revisaojs

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -126,9 +126,18 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 
 // EXERCÍCIO 11
-function retornaChamadaDeFilme(filme) {
+function retornaChamadaDeFilme(filme){
+        
+    const frase = (`Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`)
+       return frase
+    }
+
+    
    
-}
+
+    
+   
+
 
 // EXERCÍCIO 12
 function retornaPessoaAnonimizada(pessoa) {
