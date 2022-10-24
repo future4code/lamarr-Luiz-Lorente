71b2c0b3a31cd542b92508727cc210f67d47566b
Luiz Rafhael <luizrafhael_1987@hotmail.com>
2022-10-23 22:09:29 -0300

    Exercicios Finalizados

--- modulo5/lista-typescript/build/exercicio4.js
+++ modulo5/lista-typescript/build/exercicio4.js
@@ -1,1 +1,23 @@
+var SETORES;
+(function (SETORES) {
+    SETORES["MARKETING"] = "marketing";
+    SETORES["VENDAS"] = "vendas";
+    SETORES["FINANCEIRO"] = "financeiro";
+})(SETORES || (SETORES = {}));
+const listaColaboradores = [
+    { nome: "Marcos", salário: 2500, setor: SETORES.MARKETING, presencial: true },
+    { nome: "Maria", salário: 1500, setor: SETORES.VENDAS, presencial: false },
+    { nome: "Salete", salário: 2200, setor: SETORES.FINANCEIRO, presencial: true },
+    { nome: "João", salário: 2800, setor: SETORES.MARKETING, presencial: false },
+    { nome: "Josué", salário: 5500, setor: SETORES.FINANCEIRO, presencial: true },
+    { nome: "Natalia", salário: 4700, setor: SETORES.VENDAS, presencial: true },
+    { nome: "Paola", salário: 3500, setor: SETORES.MARKETING, presencial: true }
+];
+const funcao4 = (lista) => {
+    const listaFiltrada = lista.filter((colaborador) => {
+        return colaborador.setor === SETORES.MARKETING && colaborador.presencial;
+    });
+    return listaFiltrada;
+};
+console.log("ex.4", funcao4(listaColaboradores));
 //# sourceMappingURL=exercicio4.js.map
--- modulo5/lista-typescript/build/exercicio5.js
+++ modulo5/lista-typescript/build/exercicio5.js
@@ -1,1 +1,21 @@
+var ROLE;
+(function (ROLE) {
+    ROLE["ADMIN"] = "admin";
+    ROLE["USER"] = "user";
+})(ROLE || (ROLE = {}));
+const usersList = [
+    { name: "Rogério", email: "roger@email.com", role: ROLE.USER },
+    { name: "Ademir", email: "ademir@email.com", role: ROLE.ADMIN },
+    { name: "Aline", email: "aline@email.com", role: ROLE.USER },
+    { name: "Jéssica", email: "jessica@email.com", role: ROLE.USER },
+    { name: "Adilson", email: "adilson@email.com", role: ROLE.USER },
+    { name: "Carina", email: "carina@email.com", role: ROLE.ADMIN }
+];
+const funcao5 = (list) => {
+    const emailsList = list
+        .filter((user) => user.role === ROLE.ADMIN)
+        .map((user) => user.email);
+    return emailsList;
+};
+console.log("ex.5", funcao5(usersList));
 //# sourceMappingURL=exercicio5.js.map
